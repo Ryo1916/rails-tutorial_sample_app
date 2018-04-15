d02ed9c69d1b14458a360a8b02157e627bc6c367
Ryo1916 <telemon1916@gmail.com>
2018-04-15 08:23:14 +0000

    Add email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates(:email, presence: true)
+  # to be downcase before save method
+  before_save { email.downcase! }
+  
+  validates :name,  presence: true, length: { maximum: 50 }
+
+  # this is constant to check email validation
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 end
