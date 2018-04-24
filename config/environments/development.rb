ff1e0b04ef7627ea2ec8bce1202f3b9547b0eec3
Ryo1916 <telemon1916@gmail.com>
2018-04-24 07:00:04 +0000

    Set mail preview

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'us-west-2.console.aws.amazon.com/cloud9/ide/e8ff9520ea0d417bb23767b6f099eb84'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
