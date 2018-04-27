2bf8443cab58aaabc827e9164af1cd093101a87f
Ryo1916 <telemon1916@gmail.com>
2018-04-27 08:41:59 +0000

    Change password_resets routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -14,5 +10,8 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :password_resets,     only: [:new, :crreate, :edit, :update]
+  
+  # For details on the DSL available within this file, 
+  # see http://guides.rubyonrails.org/routing.html
 end
