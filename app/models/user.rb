7ff6b9bfcec3eae39ea14ab93da92e7a3c452832
Ryo1916 <telemon1916@gmail.com>
2018-05-01 05:27:58 +0000

    Add user/relationship associations and validations in relationship.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # DB associations
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   
   # DB上に不要な属性を作成するために使用
   attr_accessor :remember_token, :activation_token, :reset_token
